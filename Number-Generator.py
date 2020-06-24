0ffb4855a2a247eb1819ceadb9d50e7ec4edd20a
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-24 15:07:22 +0200

    fixed bugs

--- Number-Generator.py
+++ Number-Generator.py
@@ -7,7 +7,9 @@ def game(num_of_nums):
 
     while done:
     
-    num1 = random.randint(1, 10)
-    num2 = random.randint(1, 10)
-    num3 = random.randint(1, 10)
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+        num3 = random.randint(1, 10)
+
+        print(f"{num1} {num2} {num3}")
     
