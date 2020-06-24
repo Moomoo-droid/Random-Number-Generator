69ccf65e357b2d71a47e9060c86b6a57aa83b097
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-24 15:19:08 +0200

    Added customization

--- Number-Generator.py
+++ Number-Generator.py
@@ -1,15 +1,34 @@
 import random
 import time
 
-def game(num_of_nums):
+def game(num_of_nums, length):
 
-    done = 5
+    done = length
 
     while done:
     
-        num1 = random.randint(1, 10)
-        num2 = random.randint(1, 10)
-        num3 = random.randint(1, 10)
+        if num_of_nums == 1:
+            num1 = random.randint(1, 10)
+            print(f"{num1}")
 
-        print(f"{num1} {num2} {num3}")
-    
+        if num_of_nums == 2:
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1, 10)
+            print(f"{num1} {num2}")
+
+        if num_of_nums == 3:
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1, 10)
+            num3 = random.randint(1, 10)
+            print(f"{num1} {num2} {num3}")
+
+        if num_of_nums == 4:
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1, 10)
+            num3 = random.randint(1, 10)
+            num4 = random.randint(1, 10)
+            print(f"{num1} {num2} {num3} {num4}")
+        
+        done -= 1
+
+game(1, 1)
